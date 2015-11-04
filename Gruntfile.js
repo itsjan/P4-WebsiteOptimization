a730171f563e8672df1f7b9a794ce550d008fc7f
Jan K <jan.kaiponen@gmail.com>
2015-11-04 13:27:01 +0200

    imagefiles

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,11 +6,57 @@ module.exports = function (grunt) {
         //concat: {
         //    // 2. Configuration for concatinating files goes here.
         //}
+
+        responsive_images: {
+            dynamic_img: {
+                options: {engine: 'im',
+                    newFilesOnly: true,
+                    sizes: [{
+                            name: 'small',
+                            width: '30%',
+                            suffix: '_small',
+                            quality: 20
+                        }, {
+                            name: 'large',
+                            width: '50%',
+                            suffix: '_large',
+                            quality: 40
+                        }]},
+                files: [{
+                        expand: true,
+                        cwd: 'private/img/',
+                        src: ['**/*.{jpg,gif,png}'],
+                        dest: 'img/'
+                    }]
+            },
+            dynamic_views: {
+                options: {engine: 'im',
+                    newFilesOnly: true,
+                    sizes: [{
+                            name: 'small',
+                            width: '30%',
+                            suffix: '_small',
+                            quality: 20
+                        }, {
+                            name: 'large',
+                            width: '50%',
+                            suffix: '_large',
+                            quality: 40
+                        }]},
+                files: [{
+                        expand: true,
+                        cwd: 'private/views/images/',
+                        src: ['**/*.{jpg,gif,png}'],
+                        dest: 'views/images/'
+                    }]
+            }
+
+        },
         imagemin: {
             dynamic_img: {
                 files: [{
                         expand: true,
-                        cwd: 'private/img/',
+                        cwd: 'img/',
                         src: ['**/*.{png,jpg,gif}'],
                         dest: 'img/'
                     }]
@@ -18,22 +64,24 @@ module.exports = function (grunt) {
             dynamic_views: {
                 files: [{
                         expand: true,
-                        cwd: 'private/views/images/',
+                        cwd: 'views/images/',
                         src: ['**/*.{png,jpg,gif}'],
                         dest: 'views/images/'
                     }]
             }
         }
+
     });
 
     // 3. Where we tell Grunt we plan to use this plug-in.
     // grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-imagemin');
+    grunt.loadNpmTasks('grunt-responsive-images');
 
     // 4. Where we tell Grunt what to do when we type "grunt" into the terminal.
     // grunt.registerTask('default', ['concat']);
     // grunt.registerTask('default', ['concat', 'uglify', 'imagemin']);
-    grunt.registerTask('default', ['imagemin']);
+    grunt.registerTask('default', ['responsive_images', 'imagemin']);
 
 };
 
